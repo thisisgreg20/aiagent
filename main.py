ffb4466556e534ee2cb6b99a1fe992a5ea5ba3f6
Greg Johnson <thisisgreg2.0@gmail.com>
2026-05-10 22:09:29 -0400

    end of day push

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+def main():
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+    if response.usage_metadata is None:
+        raise RuntimeError ("Failed API request. Please try again.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
